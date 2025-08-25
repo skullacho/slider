f142fa714229b4c642f525dc26dfe606345e6c71
skullacho <pedrosmbsilva@gmail.com>
2025-08-25 09:45:07 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Xuxa1 from './assets/notebook-xuxa.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,18 +9,37 @@ function App() {
     <>
       <main>
         <header></header>
-        <section className="produtos"></section>
+        <section className="produtos">
 
-        {/*Produto 1*/}
-        <div className="card">
-          <img src="" alt="" />
-          <h2></h2>
-          <p></p>
-          <p></p>
-          <div className="avaliacao"></div>
-        </div>
-        
-        
+          {/* Produto 1 */}
+          <div className="card">
+            <img src={Xuxa1} alt="" className="img-card" />
+            <h2 className="titulo-card">Notebook Xuxa</h2>
+            <p className="desc">Notebook Gamer com RGB 1 nucleo e 512Mb de Ram</p>
+            <p className="preco">R$ 5.999,99</p>
+            <div className="avaliacao"> ★ ★ ★ ★ ☆</div>
+            <div className="off">- 10%</div>
+          </div>
+          {/* Produto 2 */}
+          <div className="card">
+            <img src={Xuxa1} alt="" className="img-card" />
+            <h2 className="titulo-card">Notebook Xuxa</h2>
+            <p className="desc">Notebook Gamer com RGB 1 nucleo e 512Mb de Ram</p>
+            <p className="preco">R$ 5.999,99</p>
+            <div className="avaliacao"> ★ ★ ★ ★ ☆</div>
+            <div className="off">- 10%</div>
+          </div>
+          {/* Produto 3 */}
+          <div className="card">
+            <img src={Xuxa1} alt="" className="img-card" />
+            <h2 className="titulo-card">Notebook Xuxa</h2>
+            <p className="desc">Notebook Gamer com RGB 1 nucleo e 512Mb de Ram</p>
+            <p className="preco">R$ 5.999,99</p>
+            <div className="avaliacao"> ★ ★ ★ ★ ☆</div>
+            <div className="off">- 10%</div>
+          </div>
+
+        </section>
         <section className="destaque"></section>
         <footer></footer>
       </main>
